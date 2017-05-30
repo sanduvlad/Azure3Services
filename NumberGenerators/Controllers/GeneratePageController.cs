7d4b77ddbd26738ced4b3c8e815d430118f160b8
sandu vlad <sandu_vlad79@yahoo.com>
2017-05-30 15:21:06 +0300

    changes

--- NumberGenerators/Controllers/GeneratePageController.cs
+++ NumberGenerators/Controllers/GeneratePageController.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace NumberGenerators.Controllers
@@ -32,7 +33,7 @@ namespace NumberGenerators.Controllers
                 return View();
         }
 
-        [HttpPost]
+        [System.Web.Http.HttpPost]
         public ActionResult GenerateNumbers()
         {
 
